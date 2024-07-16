8fdcf779002c12c67b50fef29552c3db0f3c8dcb
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-07-16 17:23:01 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 120 ms, Memory: 8892 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int K, W, H;
+    cin >> K;
+    cin >> W >> H;
+    
+    vector<vector<int>> map(H, vector<int>(W));
+    vector<vector<vector<int>>> check(H, vector<vector<int>>(W, vector<int>(K+1, W*H)));
+    
+    for (int i = 0; i < H; ++i)
+        for (int j = 0; j < W; ++j)
+            cin >> map[i][j];
+    
+    queue<tuple<int, int, int>> q;
+    check[0][0][0] = 0;
+    q.push({0, 0, 0});
+    
+    int dx[12] = {-1, -2, -2, -1, 1, 2, 2, 1, -1, 0, 1, 0};
+    int dy[12] = {-2, -1, 1, 2, -2, -1, 1, 2, 0, -1, 0, 1};
+    
+    while (!q.empty()) {
+        auto [x, y, t] = q.front();
+        q.pop();
+        
+        if (x == H - 1 && y == W - 1) {
+            cout << check[x][y][t];
+            return 0;
+        }
+        
+        for (int i = 0; i < 12; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int nt = t + (i < 8 ? 1 : 0);
+            
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+            if (nt > K) continue;
+            if (map[nx][ny] == 1) continue;
+            if (check[nx][ny][nt] <= check[x][y][t] + 1) continue;
+            
+            q.push({nx, ny, nt});
+            check[nx][ny][nt] = check[x][y][t] + 1;
+        }
+    }
+    
+    cout << -1;
+    return 0;
+}
