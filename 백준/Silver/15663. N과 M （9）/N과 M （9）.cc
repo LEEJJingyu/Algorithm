9c7c44c36e8839dc574c941438189ee1e483bad5
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-07-24 23:32:53 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,44 @@
+# include<iostream>
+using namespace std;
+int n,m;
+int arr[9];
+int num[9];
+bool isuse[9];
+void func(int k)
+{
+    if(k==m)
+    {
+        for(int i=0;i<m;i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return ;
+    }
+    int tmp = 0;
+    for(int i=0;i<n;i++){
+        if(!isuse[i]&&tmp!=num[i])
+        {
+            isuse[i]=true;
+            arr[k]=num[i];
+            tmp=arr[k];
+            func(k+1);
+            isuse[i]=false;
+        }
+    }
+}
+
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n>>m;
+    for(int i=0;i<n;i++)cin>>num[i];
+    for(int i=0;i<n;i++)
+        for(int j=n-1;j>i;j--)
+            if(num[i]>num[j]){
+    			int temp=num[i];
+            	num[i]=num[j];
+                num[j]=temp;
+            }
+    func(0);
+}
