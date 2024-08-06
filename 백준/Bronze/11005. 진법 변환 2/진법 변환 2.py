fb53bc4bd505c79129ffaa1e77f87c4bcd7c6f83
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-06 17:52:57 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,31 +1,19 @@
-def power(N,B):
-    if N < B:
-        return p.append(N)
-    p.append(N % B)
-    N = N // B
-    return  power(N,B)
-
-
-
-
-
 N, B = map(int, input().split())
 
 numbers = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-p = []
-# while N > B:
-#
-#      p.append(N % B)
-#      N = (N // B)
-#
-#
-#
-# p.append(N)
-power(N,B)
 
+p = []
+while N >= B:
+
+     p.append(N % B)
+     N = (N // B)
+
+
+
+p.append(N)
 num = p[::-1]
 for i in num:
     number=numbers[i]
-
+    
     print(number,end = '')
