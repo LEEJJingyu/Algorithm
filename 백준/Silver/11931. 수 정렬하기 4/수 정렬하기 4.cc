5bb97071eda28523696e661b58e1dc6398bc7e40
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-14 21:49:17 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 188 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <climits>
+using namespace std;
+int N;
+bool cnt[2000001] = {0,};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int t;
+        cin >> t;
+        cnt[t+1000000]=true;
+    }
+    for (int i = 2000000;i>=0; i--)
+    {
+        if(cnt[i])
+            cout << i-1000000 << '\n';
+    }
+}
