9ea45c286f0d06d9de8b2f6cd1056cc978fcbccf
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-20 22:44:17 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cstring>
+using namespace std;
+int arr[1000];
+int N;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	arr[0] = 1;
+	arr[1] = 3;
+	for (int i = 2; i < N; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2]*2;
+		arr[i] %= 10007;
+	}
+	cout << arr[N - 1];
+}
