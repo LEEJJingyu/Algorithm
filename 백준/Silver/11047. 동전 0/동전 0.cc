3a1907f985439949825470188d661d8320c8278d
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-23 08:03:56 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -11,14 +11,10 @@ int main() {
     for (int i = 0; i < N; i++)
         cin >> coin[i];
     int cnt = 0;
-    for (int i = N - 1; i >= 1; i--)
+    for (int i = N - 1; i >= 0; i--)
     {
-        while (coin[i] <= K && K > coin[i - 1])
-        {
-            K -= coin[i];
-            cnt++;
-        }
+        cnt+=K/coin[i];
+        K %= coin[i];
     }
-    cnt += K;
     cout << cnt;
 }
