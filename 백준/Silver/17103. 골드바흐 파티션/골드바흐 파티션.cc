29bc2553bc7e59dbe60d5846d8bc9744aa36f2a1
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-24 20:12:09 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 12 ms, Memory: 3040 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<set>
+#include<tuple>
+using namespace std;
+int T, N;
+
+vector<int> primes;
+vector<bool> state(1000001, true);
+void func()
+{
+    state[1] = false;
+    for (int i = 2; i * i <= 1000000; i++)
+    {
+        if (!state[i])continue;
+        for (int j = i*i; j <= 1000000; j+=i)
+            state[j] =false;
+    }
+    for (int i = 2; i <= 1000000; i++)
+        if (state[i])primes.push_back(i);
+}
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    func();
+    while (T--)
+    {
+        cin >> N;
+        int cnt = 0;
+        for (int p : primes) {
+            if (p > N/2)break;
+            if (state[N - p])
+            {
+                cnt++;
+            }
+        }
+        cout << cnt<< '\n';
+    }
+}
