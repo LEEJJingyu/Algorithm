21e0a0d207b302a1cbb865f8b26ed19313c87f39
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-08-27 20:14:19 +0900

    [Silver II] Title: 좌표 압축, Time: 440 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -8,31 +8,20 @@
 #include <list>
 using namespace std;
 
-int N;
-int cnt = 0;
-int arr1[1000001];
-int arr2[1000001];
-map<int, int> m;
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> arr1[i];
-        arr2[i] = arr1[i];
-    }
-    sort(arr2, arr2 + N);
-    
-    for (int i = 0; i < N; i++)
-    {
-        if (m.find(arr2[i]) == m.end())
-        {
-            m[arr2[i]] = cnt;
-            cnt++;
-        }
-    }
-    for (int i = 0; i < N; i++)
-    {
-        cout << m[arr1[i]] << ' ';
-    }
+int n;
+int x[1000005];
+vector<int> uni; // unique
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> x[i];
+    uni.push_back(x[i]);
+  }
+  sort(uni.begin(), uni.end());
+  uni.erase(unique(uni.begin(), uni.end()), uni.end());
+  for(int i = 0; i < n; i++)
+    cout << lower_bound(uni.begin(), uni.end(), x[i]) - uni.begin() << ' ';
 }
