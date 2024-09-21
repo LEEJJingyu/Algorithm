5ac6a41d3d610021223dc327659169d42267bfbf
LEEJJingyu <38980021+LEEJJingyu@users.noreply.github.com>
2024-09-21 11:27:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <tuple>
+#include <set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+}
